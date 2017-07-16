e7e53ea8836883a598ff28de94585b4bfff6e8b4
Alex Parloti <parloti@gmail.com>
2017-07-16 11:31:08 +0200

    Set default styleExt as SCSS

--- src/app/search-result-item/search-result-item.component.ts
+++ src/app/search-result-item/search-result-item.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-result-item',
   templateUrl: './search-result-item.component.html',
-  styleUrls: ['./search-result-item.component.css']
+  styleUrls: ['./search-result-item.component.scss']
 })
 export class SearchResultItemComponent implements OnInit {
 
--- src/app/search-result-list/search-result-list.component.ts
+++ src/app/search-result-list/search-result-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-result-list',
   templateUrl: './search-result-list.component.html',
-  styleUrls: ['./search-result-list.component.css']
+  styleUrls: ['./search-result-list.component.scss']
 })
 export class SearchResultListComponent implements OnInit {
   results: Array<any>;
